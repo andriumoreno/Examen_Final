217992691c7b40a5b604631606c08b1175a4f0a2
andriumoreno <36716912+andriumoreno@users.noreply.github.com>
2019-05-07 15:09:22 -0600

    Update prueba.cpp

--- prueba.cpp
+++ prueba.cpp
@@ -17,8 +17,6 @@ struct node
 	struct node *left, *right;
 };
 
-/* Helper function that allocates a new node with the given key and
-	NULL left and right pointers. */
 struct node* newNode(int key)
 {
 	struct node* node = (struct node*)malloc(sizeof(struct node));
@@ -27,8 +25,7 @@ struct node* newNode(int key)
 	return (node);
 }
 
-// A utility function to right rotate subtree rooted with y
-// See the diagram given above.
+
 struct node *rightRotate(struct node *x)
 {
 	struct node *y = x->left;
@@ -37,8 +34,7 @@ struct node *rightRotate(struct node *x)
 	return y;
 }
 
-// A utility function to left rotate subtree rooted with x
-// See the diagram given above.
+
 struct node *leftRotate(struct node *x)
 {
 	struct node *y = x->right;
@@ -47,87 +43,79 @@ struct node *leftRotate(struct node *x)
 	return y;
 }
 
-// This function brings the key at root if key is present in tree.
-// If key is not present, then it brings the last accessed item at
-// root. This function modifies the tree and returns the new root
+
 struct node *splay(struct node *root, int key)
 {
-	// Base cases: root is NULL or key is present at root
+	
 	if (root == NULL || root->key == key)
 		return root;
 
-	// Key lies in left subtree
+	
 	if (root->key > key)
 	{
-		// Key is not in tree, we are done
 		if (root->left == NULL) return root;
 
-		// Zig-Zig (Left Left)
+		
 		if (root->left->key > key)
 		{
-			// First recursively bring the key as root of left-left
+			
 			root->left->left = splay(root->left->left, key);
 
-			// Do first rotation for root, second rotation is done after else
+		
 			root = rightRotate(root);
 		}
-		else if (root->left->key < key) // Zig-Zag (Left Right)
+		else if (root->left->key < key) 
 		{
-			// First recursively bring the key as root of left-right
+			
 			root->left->right = splay(root->left->right, key);
 
-			// Do first rotation for root->left
 			if (root->left->right != NULL)
 				root->left = leftRotate(root->left);
 		}
 
-		// Do second rotation for root
 		return (root->left == NULL)? root: rightRotate(root);
 	}
-	else // Key lies in right subtree
+	else 
 	{
-		// Key is not in tree, we are done
+	
 		if (root->right == NULL) return root;
 
-		// Zig-Zag (Right Left)
+	
 		if (root->right->key > key)
 		{
-			// Bring the key as root of right-left
+			
 			root->right->left = splay(root->right->left, key);
 
-			// Do first rotation for root->right
+			
 			if (root->right->left != NULL)
 				root->right = rightRotate(root->right);
 		}
-		else if (root->right->key < key)// Zag-Zag (Right Right)
+		else if (root->right->key < key)
 		{
-			// Bring the key as root of right-right and do first rotation
+			
 			root->right->right = splay(root->right->right, key);
 			root = leftRotate(root);
 		}
 
-		// Do second rotation for root
+		
 		return (root->right == NULL)? root: leftRotate(root);
 	}
 }
 
-// Function to insert a new key k in splay tree with given root
 struct node *insert(struct node *root, int k)
 {
-	// Simple Case: If tree is empty
+	
 	if (root == NULL) return newNode(k);
 
-	// Bring the closest leaf node to root
+	
 	root = splay(root, k);
 
-	// If key is already present, then return
+	
 	if (root->key == k) return root;
 
-	// Otherwise allocate memory for new node
 	struct node *newnode = newNode(k);
 
-	// If root's key is greater, make root as right child
-	// of newnode and copy the left child of root to newnode
+
 	if (root->key > k)
 	{
 		newnode->right = root;
@@ -135,8 +123,6 @@ struct node *insert(struct node *root, int k)
 		root->left = NULL;
 	}
 
-	// If root's key is smaller, make root as left child
-	// of newnode and copy the right child of root to newnode
 	else
 	{
 		newnode->left = root;
@@ -144,11 +130,10 @@ struct node *insert(struct node *root, int k)
 		root->right = NULL;
 	}
 
-	return newnode; // newnode becomes new root
+	return newnode; 
 }
 
-// A utility function to print preorder traversal of the tree.
-// The function also prints height of every node
+
 void preOrder(struct node *root, int index)
 {
 
@@ -161,7 +146,7 @@ void preOrder(struct node *root, int index)
 	}
 }
 
-/* Drier program to test above function*/
+
 
 
 
